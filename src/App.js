31f3d4a30e970cc05b7494e3d214fadd4272007a
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2022-02-16 22:44:07 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = props => {
 
   const [state, setState] = useState(props);
   const { name, price} = state;
+
+  // useEffectはレンダリングの後に実行されている
+  useEffect(() => {
+    console.log('コンポーネントがレンダリングされたときや、コンポーネントの値が更新された場合に実行されている');
+  });
+
+  // 第二引数に空の配列を渡すことで、最初にレンダリングされた時のみ実行されている
+  useEffect(() => {
+    console.log('最初のレンダリング時のみ実行される');
+  }, []);
+
+  // 特定のパラメーターが描画された時や変更された場合のみ実行される
+  useEffect(() => {
+    console.log('名前が描画or更新された場合のみ実行される');
+  }, [name]);
 
   return (
     <>
