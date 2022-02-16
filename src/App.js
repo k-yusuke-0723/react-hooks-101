0305e246ad8a9b73050be45969d9f4377558571f
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2022-02-16 22:01:44 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,53 +1,30 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = props => {
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const increment2 = () => setCount(previousCount => previousCount + 1);
-  const decrement2 = () => setCount(previousCount => previousCount - 1);
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  }
 
-  const reset = () => setCount(0);
-  const double = () => setCount(count * 2);
-
-  // const threeCheckCount = () => {
-  //   if(count % 3 === 0) {
-  //     setCount(count / 3);
-  //   } else {
-  //     return
-  //   }
-  // }
-
-  const divide3 = () => setCount(previousCount => {
-    if(previousCount % 3 === 0) {
-      return previousCount / 3
-    } else {
-      return previousCount
-    }
-  })
-  
   return (
     <>
-      <div>
-        Count: {count}
-      </div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>×2</button>
-        <button onClick={divide3}>3の倍数の時だけ3で割る</button>
-      </div>
+      <p>現在の{name}は、{price}です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>RESET</button>
+      <br/>
+      <input value={name} onChange={e => setName(e.target.value)}/>    
     </>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App;
