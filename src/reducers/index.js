33a4b0139fec4a4bf3ab7c90145de3114030e1aa
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2022-02-17 09:10:29 +0900

    create reducer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,20 @@
+const events = (state = [], action) => {
+  switch(action.type) {
+    case 'CREATE_EVENT':
+      const event = { title: action.title, body: action.body };
+      const length = state.length;
+
+      // 0じゃない場合は、「length」から-1する事で最後のIDを取得することができる？
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
+
+      return [...state, { id, ...event }];
+    case 'DELETE_EVENT':
+      return state
+    case 'DELETE_ALL_EVENT':
+      return []
+    default:
+      return state
+  }
+}
+
+export default events;
